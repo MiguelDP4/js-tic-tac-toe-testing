8195da98fc67e71f33998df18e70224ab5b508db
MiguelDP4 <migueldp4@gmail.com>
2020-07-08 10:07:43 -0600

    add jest

--- src/tests/boardModule.test.js
+++ src/tests/boardModule.test.js
@@ -1,6 +1,5 @@
 const boardModule = require('../boardModule');
 
-
 test('checks if player won', () => {
   boardModule.cells = ['X', 'X', 'X', 'O', '', 'O', '', 'O', ''];
   expect(boardModule.checkWin()).toBe(true);
